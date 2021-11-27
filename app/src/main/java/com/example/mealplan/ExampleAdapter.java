16b9a2e6f3a652321bcd3c75b3e8414e8ca0bf3e
RaulAdhikari <raul.adhikari@mavs.uta.edu>
2021-11-27 17:36:17 -0600

    Initial commit

---
+++ app/src/main/java/com/example/mealplan/ExampleAdapter.java
@@ -0,0 +1,56 @@
+package com.example.mealplan;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ExampleAdapter extends RecyclerView.Adapter<ExampleAdapter.ExampleViewHolder> {
+    private ArrayList<ExampleItem> mExampleList;
+
+    public static class ExampleViewHolder extends RecyclerView.ViewHolder {
+        public TextView mTextViewLine1;
+        public TextView mTextViewLine2;
+        public TextView mTextViewLine3;
+
+        public ExampleViewHolder(View itemView) {
+            super(itemView);
+            mTextViewLine1 = itemView.findViewById(R.id.textview_line1);
+            mTextViewLine2 = itemView.findViewById(R.id.textview_line_2);
+            mTextViewLine3 = itemView.findViewById(R.id.textview_line_3);
+        }
+    }
+
+    public ExampleAdapter(ArrayList<ExampleItem> exampleList) {
+        mExampleList = exampleList;
+    }
+
+    @Override
+    public ExampleViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.example_items, parent, false);
+        ExampleViewHolder evh = new ExampleViewHolder(v);
+        return evh;
+    }
+
+    @Override
+    public void onBindViewHolder(ExampleViewHolder holder, int position) {
+        ExampleItem currentItem = mExampleList.get(position);
+
+        holder.mTextViewLine1.setText(currentItem.getLine1());
+        holder.mTextViewLine2.setText(currentItem.getLine2());
+        holder.mTextViewLine3.setText(currentItem.getLine3());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mExampleList.size();
+    }
+
+
+
+}
